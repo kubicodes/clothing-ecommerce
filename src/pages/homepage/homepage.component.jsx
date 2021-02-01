2f12f44cd7daa67b0ecd7fa2e4c522abce8f6b74
kubi <kubilayercikti1@gmail.com>
2021-02-01 21:23:09 +0100

    Created Homepage Component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,41 @@
+import './homepage.styles.scss';
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>HATS</div>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>SNEAKERS</div>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>JACKETS</div>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>T-SHIRTS</div>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <div className='title'>CAPS</div>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
