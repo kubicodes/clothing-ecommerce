8ec425577b8aa6180a5e4c63205510e54aed138e
kubi <kubilayercikti1@gmail.com>
2021-02-07 12:16:02 +0100

    Store User Data in Firebase

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -11,6 +11,32 @@ const config = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error('Error creating User', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
