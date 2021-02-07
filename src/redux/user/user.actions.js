1f5e1c51801b931505a8ce5b6c2b2a2f1c28abf0
kubi <kubilayercikti1@gmail.com>
2021-02-07 20:14:04 +0100

    Centralized userActionTypes to avoid typos

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { userActionTypes } from './user.types';
+
 export const setCurrentUser = user => ({
-  type: 'SET_CURRENT_USER', //same type what user reducer is expecting in switch case
+  type: userActionTypes.SET_CURRENT_USER, //same type what user reducer is expecting in switch case
   payload: user,
 });
