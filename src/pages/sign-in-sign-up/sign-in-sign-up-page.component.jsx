acfaa9276d3053e9e1cba0352259b341378acabc
kubi <kubilayercikti1@gmail.com>
2021-02-07 12:50:14 +0100

    Update SignIn / SignUp Page to render sign in with email and password

--- src/pages/sign-in-sign-up/sign-in-sign-up-page.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up-page.component.jsx
@@ -1,8 +1,11 @@
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
+import './sign-in-sign-up-page.styles.scss';
 const SignInSignUpPage = () => (
-  <div>
+  <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
